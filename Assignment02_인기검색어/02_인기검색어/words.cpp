63f660e57db9d3ea29e870990ffaf7e507469ca2
GrandeJota <chrismail42@gmail.com>
2021-03-11 14:29:16 +0900

    Assignment02 2021/03/11 - Most Word Version 1.1.0 => 복잡도 O(2*n+nlog n) 예상           qsort를 이용해서 검증 => case 8 시간초과

--- Assignment02_인기검색어/02_인기검색어/words.cpp
+++ Assignment02_인기검색어/02_인기검색어/words.cpp
@@ -2,66 +2,78 @@
 
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 
+#include <time.h>
 /*
 
 2021/03/10 - Most Word Version 1.0.0 => 복잡도 O(n^2+n) 예상
 										case no.10 실패 2초 초과
+2021/03/11 - Most Word Version 1.1.0 => 복잡도 O(2*n+nlog n) 예상
+										qsort를 이용해서 검증
 
 */
 
 using namespace std;
 
 #define MAX_N 1000001
-#define MAX_WORDLEN 50 // 기네스 등재 제일 긴 단어 45자(비공식 189819자)
+#define MAX_WORDLEN 256 // 기네스 등재 제일 긴 단어 45자(비공식 189819자)
 
 char input_data[MAX_N][MAX_WORDLEN] = {};
 int  record_cnt[MAX_N] = { 1 };
 bool checked_index[MAX_N] = { false };
 char output[MAX_WORDLEN] = "NONE";
 
+int compare(const void* a, const void* b)
+{
+	return strcmp((char*)a, (char*)b);
+}
+
 void word_sorting(int n)
 {
-	int j = 0;
-	int half_over_n = (int)((n / 2) + 1);
+	int cnt = 1;
+	int half_over_n = (int)((n / 2));
 	for (int i = 0; i < n; i++)
 	{
 		scanf("%s", input_data[i]);
 		//printf("%s\n", input_data[i]);
 	}
-	for (int j = 0; j < n; j++)
+	qsort(input_data, n, sizeof(input_data[0]), compare);
+	for (int i = 1; i < n; i++)
 	{
-		if (checked_index[j] == true)
-			continue;
-
-		for (int i = 1; i < n; i++)
+		if (!compare(input_data[i], input_data[i - 1]))
 		{
-			if (!strcmp(input_data[j], input_data[i]))
-			{
-				//printf("index: %d[%s] record: %d\n", i, input_data[j], record_cnt[j]+1);
-				checked_index[i] = true;
-				record_cnt[j]++;
-				if (record_cnt[j]+1 > half_over_n)
-				{
-					strcpy(output, input_data[j]);
-					return;
-				}
-			}
+			cnt++;
+		}
+		else
+		{
+			cnt = 1;
+		}
+
+		if (cnt >= half_over_n)
+		{
+			strcpy(output, input_data[i]);
+			return;
 		}
 	}
 }
 
 int main()
 {
+	clock_t start, end;
 	int n;
 	char word_n1[MAX_WORDLEN] = {};
 
+	start = clock();
 	freopen("words.inp", "r", stdin);
 	freopen("words.out", "w", stdout);
 
 	scanf("%d", &n);
 	word_sorting(n);
 	printf("%s\n", output);
+
+	end = clock();
+	printf("%d msec\n", end-start);
 
 	return 0;
 }
