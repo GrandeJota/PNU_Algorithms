a3220a60d98dbf758c058648f689e21143790f67
GrandeJota <chrismail42@gmail.com>
2021-04-02 21:24:42 +0900

    Assignment07 - 시작

---
+++ Assignment07_JugGame/07_JugGame/jug.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+#define ASCII_F 0x46 //70
+#define ASCII_S 0x53 //83
+
+int main()
+{
+	int n;
+
+	freopen("jug.inp", "r", stdin);
+	freopen("jug.out", "w", stdout);
+
+	return 0;
+}
